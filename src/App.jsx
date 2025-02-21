ba902af1e082abcd8cf3c978f85a555f55d2c247
eduarda-pimentel <eduarda.pimentel001@gmail.com>
2025-02-21 10:37:18 -0300

    final fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import './App.css';
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { AppProvider } from './AppContext';
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { AppProvider } from './AppContext'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/landingPage/landingPage';
 import Main from './pages/main/main';
 import Surpresa from './pages/surpresa/surpresa';
@@ -11,16 +11,16 @@ function App() {
   return (
     <AppProvider>
       <div className='p-0 m-0' style={{ width: '100vw', height: '100vh' }}>
-        <HashRouter>
+      <BrowserRouter basename='/projeto-momo/'>
           <Routes>
-            <Route index path="/" element={<Login />} />
-            <Route path="/main" element={<Main />} />
-            <Route path="/surpresa" element={<Surpresa />} />
+            <Route index path="/" element={<Login/>}/>
+            <Route path="/main" element={<Main/>}/>
+            <Route path="/surpresa" element={<Surpresa/>}/>
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
       </div>
     </AppProvider>
-  );
+  )
 }
 
-export default App;
+export default App
